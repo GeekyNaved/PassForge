9c00dfec8a18fea04fffe8aee8566c4695641fa5
GeekyNaved <navedahmed040@gmail.com>
2024-03-14 16:44:16 +0530

    adding desktop UI

--- src/app/page.js
+++ src/app/page.js
@@ -1,99 +1,27 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Pass Forge</h1>
-      <p>A password generator app.</p>
-    </main>
-    // <main className={styles.main}>
-    //   <div className={styles.description}>
-    //     <p>
-    //       Get started by editing&nbsp;
-    //       <code className={styles.code}>src/app/page.js</code>
-    //     </p>
-    //     <div>
-    //       <a
-    //         href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         By{" "}
-    //         <Image
-    //           src="/vercel.svg"
-    //           alt="Vercel Logo"
-    //           className={styles.vercelLogo}
-    //           width={100}
-    //           height={24}
-    //           priority
-    //         />
-    //       </a>
-    //     </div>
-    //   </div>
-
-    //   <div className={styles.center}>
-    //     <Image
-    //       className={styles.logo}
-    //       src="/next.svg"
-    //       alt="Next.js Logo"
-    //       width={180}
-    //       height={37}
-    //       priority
-    //     />
-    //   </div>
-
-    //   <div className={styles.grid}>
-    //     <a
-    //       href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className={styles.card}
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2>
-    //         Docs <span>-&gt;</span>
-    //       </h2>
-    //       <p>Find in-depth information about Next.js features and API.</p>
-    //     </a>
-
-    //     <a
-    //       href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className={styles.card}
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2>
-    //         Learn <span>-&gt;</span>
-    //       </h2>
-    //       <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-    //     </a>
-
-    //     <a
-    //       href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className={styles.card}
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2>
-    //         Templates <span>-&gt;</span>
-    //       </h2>
-    //       <p>Explore starter templates for Next.js.</p>
-    //     </a>
-
-    //     <a
-    //       href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className={styles.card}
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2>
-    //         Deploy <span>-&gt;</span>
-    //       </h2>
-    //       <p>
-    //         Instantly deploy your Next.js site to a shareable URL with Vercel.
-    //       </p>
-    //     </a>
-    //   </div>
-    // </main>
+    <div class={styles.main}>
+      <div class={styles.card}>
+        <div class={styles.cardBody}>
+          <h3 class={`${styles["cardTitle"]} ${styles["fields"]}`}>Password Generator</h3>
+          <div class={styles.fields}>
+            <input type="text" id="input" class={styles.plainTxt} placeholder="Enter plain text here"
+              required />
+            <textarea rows="8" id="output" class={styles.encryptedTxt}
+              placeholder="Encrypted Password will come here" />
+            <div class={styles.copyContainer}>
+              <a class={`${styles["btn"]} ${styles["copy"]}`} href="#" id="copy">copy</a>
+            </div>
+          </div>
+          <div class={styles.btnContainer}>
+            <a class={`${styles["btn"]} ${styles["encrypt"]}`} href="#" id="encrypt">Generate (Encrypt)</a>
+            <a class={`${styles["btn"]} ${styles["decrypt"]}`} href="#" id="decrypt">Decrypt (Generate original text)</a>
+            <a class={`${styles["btn"]} ${styles["clear"]}`} href="#" id="clear"> Clear Text</a>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
